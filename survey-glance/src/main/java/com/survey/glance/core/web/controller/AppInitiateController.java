4261848fe9239a87d4d9ecde47d90c02ec01271c
Administrator <Administrator@balu-PC>
2017-06-21 09:46:58 +0530

    no change

--- survey-glance/src/main/java/com/survey/glance/core/web/controller/AppInitiateController.java
+++ survey-glance/src/main/java/com/survey/glance/core/web/controller/AppInitiateController.java
@@ -32,7 +32,7 @@ public class AppInitiateController {
 
 	/**
 	 * @param model
-	 * @return
+	 * @return String
 	 */
 	@RequestMapping(value = { "/" }, method = RequestMethod.GET)
 	public String defalutPage(ModelMap model) {
